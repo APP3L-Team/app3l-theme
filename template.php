cfc889bdf65fd9a7b95d8866514ae1d9971852aa
operandom <dev@operandom.com>
2014-06-20 21:24:07 +0200

    Move twig system in engine

--- template.php
+++ template.php
@@ -1,17 +1,2 @@
 <?php
 
-require_once 'vendor/autoload.php';
-
-use libs\TwigProxy as Twig;
-
-function app3l_html(&$vars)
-{
-	$twig = new Twig();
-	return  $twig->render('layout.html.twig', $vars);
-}
-
-function app3l_page(&$vars)
-{
-	$twig = new Twig();
-	return  $twig->render('page.html.twig', $vars);
-}
